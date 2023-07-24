53b63f1b0ddf397d64a1c165eada662f386ff0f4
gavindennis2000 <gavinferraridennis@gmail.com>
2023-07-24 00:19:59 -0500

    edited CSS

--- proteinpricer.js
+++ proteinpricer.js
@@ -1,31 +1,38 @@
 function findProteinCost() {
+
     // first, retrieve the necessary info from the HTML user input
     let name = document.querySelector("#foodName");
     let protein = document.querySelector("#protein");
     let price = document.querySelector("#price");
     let servings = document.querySelector("#servings");
 
-    // second, check to make sure protein, price, and servings are all numerical values (no letters nor characters!)
-    if (isNaN(protein.value)) {
-        message.innerHTML = ("Protein value must be a number (e.g. 15). Please try again and make sure no letters or symbols are used!!");
-        return 0;
-    }
 
-    if (isNaN(price.value)) {
+    // second, check to make sure protein, price, and servings are all numerical values (no letters nor characters!)
+    if (name.value == "") {
+        name.value = "food item";  }
+
+    if (isNaN(protein.value) || protein.value == "") {
+        message.innerHTML = ("Protein value must be a number (e.g. 15). Please try again and make sure no letters or symbols are used!!");
+        return 0; }
+
+    if (isNaN(price.value) || price.value == "") {
         message.innerHTML = ("Price value must be a number (e.g. 5.99). Please try again and make sure no letters or symbols are used!!");
-        return 0;
-    }
+        return 0; }
 
     if (isNaN(servings.value)) {
         message.innerHTML = ("Servings value must be a number (e.g. 11). Please try again and make sure no letters or symbols are used!!");
-        return 0;
-    }
+        return 0;  }
+    
+    if (servings.value == "" || servings.value == 0) {
+        servings.value = 1; }
+
 
     // third, use these values to calculate the important stuff
     pricePerServing = (price.value / servings.value);
     proteinCost = (pricePerServing / protein.value).toFixed(2);
     proteinPerDollar = (protein.value * servings.value / price.value).toFixed(1);
     ratingTemp = (proteinPerDollar*2).toFixed(0);
+
 
     // fourth, find the Protein Pricer Rating using the temp variable
     if (ratingTemp >= 20 && ratingTemp < 50) {
@@ -37,12 +44,12 @@ function findProteinCost() {
     else if (ratingTemp >= 100) {
         proteinPricerRating = "Remarkable!!!"; }
     else
-        proteinPricerRating = "Not Very Good!"
+        proteinPricerRating = "Not Good"
 
     // finally, print the results to the webpage
     message.innerHTML = (
-        "1g of protein from " + name.value + " costs $" + proteinCost + ".<br/>" +
+        "1g of protein from " + name.value + " costs $" + proteinCost + ".<br/><br/>" +
         "One dollar's worth of " + name.value + " yields " + proteinPerDollar + "g of protein.<br/><br/>" +
-        "The Protein Pricer Rating is: " + proteinPricerRating + "<br/><br/>"
+        "The Protein Pricer Rating is: " + proteinPricerRating
     )
 }
